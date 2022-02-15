fd4f14d0e662421f320b918fabaa6f29d546544e
e1turin <ivturnik2003@gmail.com>
2022-02-15 20:17:55 +0300

    added tree.sh in project

--- tools/tree.sh
+++ tools/tree.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/bash
 path=$1
 if [ -z "$path"]
 then
     path=.
 fi
 
-find $path | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
+find $path | sed -e 's;[^/]*/;∟____;g;s;____∟; |;g'
